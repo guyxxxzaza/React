95187f892dad6c8ba736aae64de42e61dd6478fa
guyxxxzaza <guy_akkaraphol@hotmail.com>
2018-09-13 10:30:06 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import './CharacterCard';
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
  }
 }
 export default App;
